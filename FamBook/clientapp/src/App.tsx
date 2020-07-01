714f68203156aa5038db13c0d2a24c069825d4bb
keseray <aycankeser@gmail.com>
2020-07-01 16:22:00 +0200

    Initial commit

---
+++ FamBook/clientapp/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./App.css";
+import AppRouter from "./router";
+const App: React.FC = () => {
+    return (
+        <AppRouter />
+    );
+}
+export default App;
