df679fea9064abe1effb4df5e6a5fb77a7374f3e
keseray <aycankeser@gmail.com>
2020-08-13 12:03:55 +0200

    Add Weather UI

--- FamBook/clientapp/src/components/Home.tsx
+++ FamBook/clientapp/src/components/Home.tsx
@@ -1,28 +1,65 @@
 ﻿import React from 'react';
-import logo from '../logo.svg';
+import Form from './Form';
+import WeatherDetails from './WeatherDetails';
+import { Weather } from '../types/Weather';
+import { Country } from '../types/Country';
+import { City } from '../types/City';
+import { Constants } from '../Constants';
+interface IState {
+    weather: Weather,
+    countries: Country[],
+    city?: City
+}
+
 
 class Home extends React.Component {
 
+    public state: IState = {
+        weather: {
+            error: ""
+        } as Weather,
+        countries: [],
+        city: undefined
+    }
+
+    async getCountries(): Promise<Country[]> {
+        try {
+            const res = await fetch
+                (`${Constants.locationAPIUrl}/countries?apikey=${Constants.apiKey}`);
+            return await res.json() as Country[];
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            const countries = await this.getCountries();
+            await this.setStateAsync({ countries: countries } as IState);
+        } catch (error) {
+        }
+    }
+
+    async setStateAsync(state: IState) {
+        return new Promise((resolve: any) => {
+            this.setState(state, resolve);
+        });
+    }
+
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+            <div className="container content panel">
+                <div className="container">
+                    <div className="row">
+                        <div className="form-container">
+                            <WeatherDetails weather={this.state.weather} />
+                            <Form countries={this.state.countries} />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
-
 export default Home;
---
+++ FamBook/clientapp/src/types/City.ts
@@ -0,0 +1,7 @@
+﻿import { Country } from './Country';
+export interface City {
+    Key: string;
+    EnglishName: string;
+    Type: string;
+    Country: Country;
+};
