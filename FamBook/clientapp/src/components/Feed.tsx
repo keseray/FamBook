13153d31e51a16aa0f7fb6f63c92465df3a24cf6
keseray <aycankeser@gmail.com>
2020-10-15 19:44:52 +0200

    Adding Feed.tsx

---
+++ FamBook/clientapp/src/components/Feed.tsx
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+class Feed extends React.Component {
+    props = {name : 'Aycan'}
+
+    render() {
+        return (
+            <div className="shopping-list">
+                <h1>Shopping List for {this.props.name}</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>WhatsApp</li>
+                    <li>Oculus</li>
+                </ul>
+            </div>
+        );
+    }
+}
