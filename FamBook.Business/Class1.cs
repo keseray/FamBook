a97e3e151edc4157fe151e05d7299b01652ffb30
keseray <aycankeser@gmail.com>
2020-11-09 19:23:09 +0100

    New architecture

---
+++ FamBook.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FamBook.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ FamBook.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FamBook.DataAccess
+{
+    public class Class1
+    {
+    }
+}
